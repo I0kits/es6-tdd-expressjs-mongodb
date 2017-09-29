d6dc2072ade1eca4f2a2b4dc707cccaccbdcd8db
dgchen <dgchen@thoughtworks.com>
2017-09-29 15:24:46 +0800

    [diangui] package antd icon to local

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const theme = require('./src/theme');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
@@ -51,8 +52,23 @@ module.exports = {
         test: /\.less$/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: ['css-loader', 'less-loader']
+          use: ['css-loader', {
+            loader: 'less-loader',
+            options: {
+              modifyVars: theme
+            }
+          }]
         })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader:'file-loader'
+        }
       }
     ]
   }
