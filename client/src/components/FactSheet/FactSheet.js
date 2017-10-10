83a5a8c7ec990e5fec7d81c72dd1a5eb5a6007f8
dgchen <dgchen@thoughtworks.com>
2017-10-10 09:27:24 +0800

    [diangui] adjust homepage style

--- client/src/components/FactSheet/FactSheet.js
+++ client/src/components/FactSheet/FactSheet.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Table } from 'antd';
+import { Tabs, Table, Icon } from 'antd';
 import './FactSheet.scss';
 
 const columns = [{
@@ -39,20 +39,20 @@ const columns = [{
   key: 'badge',
 }];
 const data = [];
-// for (let i = 1; i <= 10; i++) {
-//   data.push({
-//     key: i,
-//     public_time: '02, AUG, 2017',
-//     publisher: 'John Brown',
-//     strategy_type: 'strategy_type',
-//     variant: 'variant',
-//     start_date: '01, AUG, 2017',
-//     end_date: '02, AUG, 2017',
-//     creator: 'John Brown',
-//     status: '2/2',
-//     badge: 'badge',
-//   });
-// }
+for (let i = 1; i <= 10; i += 1) {
+  data.push({
+    key: i,
+    public_time: '02, AUG, 2017',
+    publisher: 'John Brown',
+    strategy_type: 'strategy_type',
+    variant: 'variant',
+    start_date: '01, AUG, 2017',
+    end_date: '02, AUG, 2017',
+    creator: 'John Brown',
+    status: '2/2',
+    badge: 'badge',
+  });
+}
 
 
 class FactSheet extends Component {
@@ -65,12 +65,18 @@ class FactSheet extends Component {
 
   render() {
     return (
-      <Tabs type="card">
-        <Tabs.TabPane tab="Draft" key="draft">
-          <Table {...this.state} columns={columns} dataSource={data} />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Published" key="published">Content of Tab Pane 2</Tabs.TabPane>
-      </Tabs>
+      <div>
+        <div className="icon-zone">
+          <span><Icon type="upload" />Publish</span>
+          <span><Icon type="download" />Download</span>
+        </div>
+        <Tabs type="card">
+          <Tabs.TabPane tab="Draft" key="draft">
+            <Table {...this.state} columns={columns} dataSource={data} />
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Published" key="published">Content of Tab Pane 2</Tabs.TabPane>
+        </Tabs>
+      </div>
     );
   }
 }
