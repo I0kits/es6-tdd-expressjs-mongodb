94db558e3f18246c08e8b03c5aff4a599457cf96
dgchen <dgchen@thoughtworks.com>
2017-10-10 18:26:10 +0800

    [diangui] overwrite output path in prod config

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -3,12 +3,14 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const output_path = path.resolve(__dirname, '../www');
 
 module.exports = merge(common,{
   devtool: 'source-map',
-
+  output: {
+    path: output_path,
+    filename: '[name].[chunkhash].bundle.js'
+  },
   plugins: [
     new UglifyJSPlugin({sourceMap: true}),
     new webpack.DefinePlugin({
@@ -20,6 +22,5 @@ module.exports = merge(common,{
     new webpack.optimize.CommonsChunkPlugin({
       name: 'common'
     }),
-    new CleanWebpackPlugin([output_path], {allowExternal: true}),
   ],
 });
