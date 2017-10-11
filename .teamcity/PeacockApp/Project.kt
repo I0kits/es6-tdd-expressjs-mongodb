2de7a0c2aaa4be227ffe405568d3e406ae14afed
CaoYY <defcyy@gmail.com>
2017-10-11 16:39:38 +0800

    teamcity version setings prefer from VCS

--- .teamcity/PeacockApp/Project.kt
+++ .teamcity/PeacockApp/Project.kt
@@ -3,6 +3,9 @@ package PeacockApp
 import PeacockApp.buildTypes.*
 import jetbrains.buildServer.configs.kotlin.v10.*
 import jetbrains.buildServer.configs.kotlin.v10.Project
+import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.VersionedSettings.*
+import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.versionedSettings
 
 object Project : Project({
     uuid = "3b3f59bc-3b03-4763-9e85-0f31a6f5ba4c"
@@ -12,4 +15,16 @@ object Project : Project({
 
     buildType(PeacockAppTest)
     buildType(PeacockAppBuild)
+
+    features {
+        versionedSettings {
+            id = "PROJECT_EXT_SETTINGS_MODE"
+            mode = VersionedSettings.Mode.ENABLED
+            buildSettingsMode = VersionedSettings.BuildSettingsMode.PREFER_SETTINGS_FROM_VCS
+            rootExtId = "Peacock_Peacock"
+            showChanges = false
+            settingsFormat = VersionedSettings.Format.KOTLIN
+            param("credentialsStorageType", "credentialsJSON")
+        }
+    }
 })
